6594df503002a6e70b1299f781b1465047674e5b
Andy Orme <andy@pixl8.co.uk>
2018-03-17 13:34:55 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(function() {
+	// prevent default behaviour of "a" tags
+
+	// user clicks ".readmore"
+		// "<p>" slides down
+		// "read less" shows
+		// "read more" hides
+
+	// user clicks ".readless"
+		// "<p>" slides up
+		// "read more" shows
+		// "read less" hides
+
+	// user clicks ".learnmore"
+		// "<span>" slides down
+		// "hide" shows
+		// "learn more" hides
+
+	// user clicks ".hide"
+		// "<span>" slides up
+		// "learn more" shows
+		// "hide" hides
+});
+
+
+
+
+
+
+
+
+
